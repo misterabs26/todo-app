ffda15097fdd92acfb26ba5c7e7807be424c7d62
ghyxen26 <ayeras.gerald26@gmail.com>
2025-02-23 22:23:29 +0800

    Implement the edit button and created a function for updating the window whenever there is changes to todo list

--- gui-todo.py
+++ gui-todo.py
@@ -4,11 +4,27 @@ import FreeSimpleGUI as sg
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter a todo", key="todo")
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=todofunc.get_todos(),
+                      key="todos_items",
+                      enable_events=True,
+                      size=(45,10))
+edit_button = sg.Button("Edit")
+
 window = sg.Window('My Todo App',
-                   layout=[[label],
-                           [input_box],
-                           [add_button]],
+                   layout=[[list_box,edit_button],
+                           [label],
+                           [input_box,add_button]],
                    font=('Helvetica',12))
+
+def update_window(window_element, updated_value=None):
+    if isinstance(updated_value, str):
+        updated_value = [updated_value]
+        return window[window_element].update(value = updated_value[0])
+    if updated_value is None:
+        updated_value = []
+
+    return window[window_element].update(values = updated_value)
+
 while True:
     event, values = window.read()
 
@@ -19,6 +35,19 @@ while True:
             todos.append(todo)
             todofunc.write_todos(todos)
             input_box.update("")
+            update_window('todos_items',todos)
+        case "Edit":
+            selected_todo = values["todos_items"][0]
+            new_todo = values["todo"]+"\n"
+            todos = todofunc.get_todos()
+            target_todo_index = todos.index(selected_todo)
+
+            todos[target_todo_index] = new_todo
+            todofunc.write_todos(todos)
+            input_box.update("")
+            update_window('todos_items',todos)
+        case 'todos_items':
+            update_window('todo', values["todos_items"][0])
         case sg.WIN_CLOSED:
             print("Bye-bye")
             break
