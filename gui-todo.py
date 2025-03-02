af786762afb098defd10fd957744d8441684f799
ghyxen26 <ayeras.gerald26@gmail.com>
2025-03-02 13:29:25 +0800

    Sanitized input fields and clean the code

--- gui-todo.py
+++ gui-todo.py
@@ -23,47 +23,53 @@ window = sg.Window('My Todo App',
                            [list_box],
                            [label],
                            [input_box],
-                           buttons],
-                   font=('Helvetica',12))
+                           [buttons]],
+                   font=('Helvetica', 12))
 
 def update_window(window_element, updated_value=None):
-    if isinstance(updated_value, str):
-        updated_value = [updated_value]
-        return window[window_element].update(value = updated_value[0])
-    if updated_value is None:
-        updated_value = []
-
-    return window[window_element].update(values = updated_value)
+    """Updates listbox and input text"""
+    element = window[window_element]
+    if isinstance(element, sg.Input):
+        element.update(value=updated_value)
+    elif isinstance(element, sg.Listbox):
+        if updated_value is None:
+            updated_value = []
+        if isinstance(updated_value, str):
+            updated_value = [updated_value]
+        element.update(values = updated_value)
 
 while True:
     event, values = window.read(timeout=200)
 
-    # will check if the window is open before updating it else, it will close
+    # Exit immediately if window is closed
     if event == sg.WIN_CLOSED or event == "Exit":
         sg.popup("Bye-bye", text_color="yellow", auto_close=True)
         break
-    if window:
-        window["clock"].update(value=time.strftime("%b, %d, %Y %H:%M:%S"))
+
+    window["clock"].update(value=time.strftime(f"%b, %d, %Y %H:%M:%S"))
 
     match event:
         case "Add":
-            todo = values["todo"] + "\n"
+            todo = values["todo"].strip()
             todos = todofunc.get_todos()
-            todos.append(todo)
+            todos.append(todo + "\n")
             todofunc.write_todos(todos)
             input_box.update("")
             update_window('todos_items',todos)
         case "Edit":
             try:
                 selected_todo = values["todos_items"][0]
-                new_todo = values["todo"]+"\n"
-                todos = todofunc.get_todos()
-                target_todo_index = todos.index(selected_todo)
+                new_todo = values["todo"].strip()
+                # checks if there is being inputted
+                if new_todo:
+                    todos = todofunc.get_todos()
+                    target_todo_index = todos.index(selected_todo)
 
-                todos[target_todo_index] = new_todo
-                todofunc.write_todos(todos)
-                input_box.update("")
-                update_window('todos_items',todos)
+                    todos[target_todo_index] = new_todo + "\n"
+                    todofunc.write_todos(todos)
+                    update_window('todos_items',todos)
+                else:
+                    sg.popup("Please enter a valid to-do item.", font=('Helvetica', 15))
             except IndexError:
                 sg.popup("Please select an item first.",
                          font=('Helvetica',15))
@@ -80,7 +86,6 @@ while True:
             except IndexError:
                 sg.popup("Please select an item first.",font=('Helvetica',15))
         case 'todos_items':
-            update_window('todo', values["todos_items"][0])
-
+            update_window('todo', values["todos_items"][0].strip())
 
 window.close()
